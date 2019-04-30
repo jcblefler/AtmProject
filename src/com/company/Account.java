3a256f5b08296811ae29c73544f91e08a1b4dd9c
bkabtiymer <bkabtiymer@gmail.com>
2019-04-30 10:31:27 -0400

    comments added

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -11,7 +11,7 @@ public class Account extends Atm {
     public Account() {
     }
 
-
+//
 
     public String displayAccount(){
         return getAccountNum() + " " + getPin() + " " + getUserName() + " " + getBalance();
--- src/com/company/Atm.java
+++ src/com/company/Atm.java
@@ -17,11 +17,11 @@ public class Atm {
     private boolean loginSuccessful;
 
 
-
+//Initialized atm class using constructor
 
     public Atm() {
     }
-
+// Ask the user if he/she has an account
     public boolean accountCheck(){
         System.out.println("Do you have an account. (y/n)");
         String answer = scanner.next();
@@ -30,7 +30,7 @@ public class Atm {
         } else{
             account = false;
         }
-
+// If user has an account, prompt login. If user doesn't have an account create one
         if(account == true){
             login();
         }else{
@@ -42,7 +42,7 @@ public class Atm {
     public void login(){
 
 
-
+//Enter user log-in account number and PIN
         loop:
         while(loginAttempts < 3) {
              System.out.println("Enter your account number: ");
@@ -50,7 +50,7 @@ public class Atm {
 
             System.out.println("Enter your account pin: ");
             int userInputPin = scanner.nextInt();
-
+//Loop through identified arrayList of accounts. If Account number and PIN  match, Log-in successful.
 
             for (int i = 0; i < users.size(); i++) {
                 if (userInputAccount == users.get(i).getAccountNum() && userInputPin == users.get(i).getPin()) {
@@ -60,48 +60,59 @@ public class Atm {
                     break loop;
                 }
             }
+//            If account number and PIN don't match, set login to false and add one to attempts and rerun loop
             loginAttempts++;
             loginSuccessful = false;
         }
+//        After 3 attempts, account is locked out
         if (loginAttempts >= 3){
             System.out.println("Your account has been locked out.");
         }
+//        If login is successful, run transaction
         if (loginSuccessful == true){
             transaction();
         }
 
      }
-
+//Prompt the user to select between withdraw, deposit or check balance options
 
      public boolean transaction(){
 
          System.out.println("Would you like to make a withdraw, deposit, or check balance.");
          String userInput = scanner.next();
-
+//If withdraw is selected, ask amount, calculate balance and print value
          if (userInput.equalsIgnoreCase("withdraw")){
              System.out.println("How much are you withdrawing.");
              double withdrawAmount = scanner.nextDouble();
              System.out.println("Your balance is now $" + df2.format(users.get(accountLocation).getBalance() - withdrawAmount));
              users.get(accountLocation).setBalance(users.get(accountLocation).getBalance() - withdrawAmount);
-         } else if (userInput.equalsIgnoreCase("deposit")){
+         }
+//         If deposit is selected, ask amount, calculate balance and print value
+         else if (userInput.equalsIgnoreCase("deposit")){
              System.out.println("How much are you depositing.");
              double depositAmount = scanner.nextDouble();
              System.out.println("Your balance is now $" + df2.format(users.get(accountLocation).getBalance() + depositAmount));
              users.get(accountLocation).setBalance(users.get(accountLocation).getBalance() + depositAmount);
-         } else if (userInput.equalsIgnoreCase("check")){
+         }
+//         If check is selected print balance amount
+         else if (userInput.equalsIgnoreCase("check")){
              System.out.println("Your balance is: $" + df2.format(users.get(accountLocation).getBalance()));
          }
+//         If user wants to make a new transaction, re-run the loop
          System.out.println("Would you like to make another transaction. (y/n)");
          String userAnswer = scanner.next();
          if (userAnswer.equalsIgnoreCase("y")){
              transaction();
-         } else{
+         }
+//         If user is finished making transactions, good-bye message alongside username is displayed
+         else{
              System.out.println("Have a nice day, " + users.get(accountLocation).getUserName() + ".\n\n");
          }
+//        Return to prompt different user to log-in
          return accountCheck();
      }
 
-
+//Method to create new user account
     public boolean createAccount(){
 
         Account user = new Account();
@@ -114,7 +125,7 @@ public class Atm {
         System.out.println("Enter your balance: ");
         user.setBalance(scanner.nextDouble());
 
-
+//Add created user to ArrayList
         users.add(user);
 
         return accountCheck();
@@ -143,5 +154,21 @@ public class Atm {
 
     public void setLoginAttempts(int loginAttempts) {
         this.loginAttempts = loginAttempts;
+    }
+
+    public int getAccountLocation() {
+        return accountLocation;
+    }
+
+    public void setAccountLocation(int accountLocation) {
+        this.accountLocation = accountLocation;
+    }
+
+    public boolean isLoginSuccessful() {
+        return loginSuccessful;
+    }
+
+    public void setLoginSuccessful(boolean loginSuccessful) {
+        this.loginSuccessful = loginSuccessful;
     }
 }
